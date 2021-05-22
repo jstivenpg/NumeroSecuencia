1d67ecf18ac72474d4df0ed155cb5c4223fbd8ee
JStivenpgSGM <43974110+jstivenpg@users.noreply.github.com>
2021-05-22 13:58:52 -0500

    Código Js

---
+++ numeros.js
@@ -0,0 +1,8 @@
+function myFunction(numeros) {
+
+    numeros = document.querySelector("#numeros").value;
+    for (let index = numeros; index > 0; index--) {
+        document.writeln(index);   
+             
+    }
+  }
